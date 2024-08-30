d5b7c7d426ebcbab6dba12b5121c8a01e73abf36
Ferchisx <fefernandeza@unal.edu.co>
2024-08-29 19:43:58 -0500

    .IOC update
    Usart1 wasn't defined, it was activated and also some parts of the main.c code was commented

--- project/Core/Src/main.c
+++ project/Core/Src/main.c
@@ -101,29 +101,29 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 	}
 }
 
-void heartbeat(void)
-{
-	static uint32_t heartbeat_tick = 0;
-	if (heartbeat_tick < HAL_GetTick()) {
-		heartbeat_tick = HAL_GetTick() + 500;
-		HAL_GPIO_TogglePin(D1_GPIO_Port, D1_Pin);
-	}
-}
-
-void turn_signal_left(void)
-{
-	static uint32_t turn_toggle_tick = 0;
-	if (turn_toggle_tick < HAL_GetTick()) {
-		if (left_toggles > 0) {
-			turn_toggle_tick = HAL_GetTick() + 500;
-			HAL_GPIO_TogglePin(D3_GPIO_Port, D3_Pin);
-			left_toggles--;
-		} else {
-			HAL_GPIO_WritePin(D3_GPIO_Port, D3_Pin, 1);
-		}
-
-	}
-}
+//void heartbeat(void)
+//{
+//	static uint32_t heartbeat_tick = 0;
+//	if (heartbeat_tick < HAL_GetTick()) {
+//		heartbeat_tick = HAL_GetTick() + 500;
+//		HAL_GPIO_TogglePin(D1_GPIO_Port, D1_Pin);
+//	}
+//}
+//
+//void turn_signal_left(void)
+//{
+//	static uint32_t turn_toggle_tick = 0;
+//	if (turn_toggle_tick < HAL_GetTick()) {
+//		if (left_toggles > 0) {
+//			turn_toggle_tick = HAL_GetTick() + 500;
+//			HAL_GPIO_TogglePin(D3_GPIO_Port, D3_Pin);
+//			left_toggles--;
+//		} else {
+//			HAL_GPIO_WritePin(D3_GPIO_Port, D3_Pin, 1);
+//		}
+//
+//	}
+//}
 /* USER CODE END 0 */
 
 /**
@@ -173,7 +173,7 @@ int main(void)
 	  uint8_t byte = 0;
 	  if (ring_buffer_is_full(&rb_usart2) != 0) {
 		  int8_t id_incorrect = 0;
-		  char my_id[] = "1053821948";
+		  char my_id[] = "1056120378";
 		  for (uint8_t idx = 0; idx < sizeof(my_id); idx++) {
 			  if (ring_buffer_read(&rb_usart2, &byte) != 0) { // 0x20
 				  if (byte != my_id[idx]) {
@@ -182,7 +182,7 @@ int main(void)
 			  }
 		  }
 		  if (id_incorrect == 0) {
-			  HAL_UART_Transmit(&huart2, "Sam C\r\n", 7, 10);
+			  HAL_UART_Transmit(&huart2, "Felipe F\r\n", 10, 10);
 		  } else {
 			  HAL_UART_Transmit(&huart2, "Error\r\n", 7, 10);
 		  }
@@ -192,8 +192,8 @@ int main(void)
 		  ring_buffer_read(&rb_usart1, &data);
 		  HAL_UART_Transmit(&huart2, &data, 1, 10);
 	  }
-	  heartbeat();
-	  turn_signal_left();
+//	  heartbeat();
+//	  turn_signal_left();
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
